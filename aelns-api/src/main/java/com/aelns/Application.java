2688567caa30a7956ff740a605e4dd3dd3b120b3
aelns <YY123123>
2017-03-11 01:12:48 +0800

    improve mybatis configuration

--- aelns-api/src/main/java/com/aelns/Application.java
+++ aelns-api/src/main/java/com/aelns/Application.java
@@ -1,9 +1,11 @@
 package com.aelns;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.aelns.dao")
 public class Application {
 
     public static void main(String[] args) {
