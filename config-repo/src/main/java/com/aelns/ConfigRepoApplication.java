4d4923d8297bf63112116dd560e9886ba91c4772
aelns <alwaysc@foxmail.com>
2017-03-21 12:58:08 +0800

    config-repo

---
+++ config-repo/src/main/java/com/aelns/ConfigRepoApplication.java
@@ -0,0 +1,12 @@
+package com.aelns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigRepoApplication.class, args);
+	}
+}
